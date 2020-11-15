5894f999f9c2817fb92b64a9c9233065e98f8c98
Tony Pham <topham@adobe.com>
2020-11-15 15:39:07 -0800

    added googleLogin and redirect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
+import { useStateProvider } from "./StateProvider";
+import { auth } from "./firebase.js";
 
 function App() {
+  const [{ authorized, user }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  console.log("user >>>", user);
   return (
     <div className="App">
       <Router>
